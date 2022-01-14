d5eb79644ae2d2d0b5155c47f2bf8fd090578219
ndxbinh1922001 <binhndx1922001@gmail.com>
2022-01-14 20:16:13 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const { extname } = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+//HTTP logger
+app.use(morgan("combined"));
+//template engine
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
