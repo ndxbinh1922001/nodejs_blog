aac9b70b52de248adcce370aec442052680b194a
ndxbinh1922001 <binhndx1922001@gmail.com>
2022-01-15 10:47:18 +0700

    third second

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,11 @@
+class SiteController {
+	//[get] /news
+	index(req, res) {
+		res.render("home");
+	}
+	//[get] /search
+	search(req, res) {
+		res.render("search");
+	}
+}
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -5,17 +5,24 @@ const path = require("path");
 const { extname } = require("path");
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //HTTP logger
 app.use(morgan("combined"));
+
 //template engine
 app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
-app.get("/", (req, res) => {
-	res.render("home");
-});
+
+//home, search, contact
+
+//routes init
+route(app);
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
